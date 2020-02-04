72f6692020a1e39e051e3585bc75367321b581f5
stacytonui <stacyanne01@gmail.com>
2020-02-04 14:37:05 +0300

    Creating Profiles

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,9 +10,9 @@ class ProfilesController extends Controller
 {
    public function index($user)
     {
-       $user = User::find($user);
-       
+       $user = User::findOrFail($user);
+
         return view('home', ['user' =>$user, ]);
-       
+
     }
 }
