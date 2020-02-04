0d10f115d8700b8d15307ef42f4cb9ddbc82c564
stacytonui <stacyanne01@gmail.com>
2020-02-04 14:54:47 +0300

    Added Posts

--- database/migrations/2020_02_04_113041_create_posts_table.php
+++ database/migrations/2020_02_04_113041_create_posts_table.php
@@ -16,8 +16,10 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
+            $table->string('caption');
+            $table->string('image');
             $table->timestamps();
-            $table->index(user_id);
+            $table->index('user_id');
 
         });
     }
