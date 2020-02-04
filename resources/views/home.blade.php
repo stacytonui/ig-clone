a87acb8b88b4ab986a4a75b025f667dc5f817ce7
stacytonui <stacyanne01@gmail.com>
2020-02-04 12:20:52 +0300

    Creating profiles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a7247d6a-142c-47a6-a615-f88f13936642/dbf8j7j-e5b54ac0-3107-4c18-aa16-d34345b90543.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E3MjQ3ZDZhLTE0MmMtNDdhNi1hNjE1LWY4OGYxMzkzNjY0MlwvZGJmOGo3ai1lNWI1NGFjMC0zMTA3LTRjMTgtYWExNi1kMzQzNDViOTA1NDMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.8XJR-6Cc31727F-QJRZrAwBpGliC2PMJCP94HLLH4Gs" height="70px">
         </div>
         <div class="col-9 p-5">
-        <div><h1>btsgram</h1></div>
+        <div><h1>{{ $user ->username}}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153 </strong>posts</div>
                 <div class="pr-5"><strong>23k </strong>followers</div>
